381e8c85f483641c105669a36465d3bf2bf3ffdd
Philip Katiku <peekaylas@gmail.com>
2025-06-27 23:26:08 +0300

    __init__.py file created

---
+++ __init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.Config')  
+    db.init_app(app)
+    jwt.init_app(app)
+
+
+    from app.routes.auth import auth_bp
+    from app.routes.playlists import playlist_bp
+    from app.routes.songs import songs_bp
+    app.register_blueprint(auth_bp, url_prefix='/api')
+    app.register_blueprint(playlist_bp, url_prefix='/api')
+    app.register_blueprint(songs_bp, url_prefix='/api')
+
+    return app
